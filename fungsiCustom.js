0aa04707a98b3639936c456af0aabcdddc89f32b
khoirul02 <hudak006@gmail.com>
2022-06-11 09:20:18 +0700

    submit v1

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = () => {
+  var hasil = [];
+  fs.readFile('./data1.json', 'utf8', (err, data1) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let Data1 = JSON.parse(data1)
+    let ambilKataBelakang1 = Data1.message.split(' ').slice(-1).join(' ');
+    hasil.push(ambilKataBelakang1);
+    fs.readFile('./data2.json', 'utf8', (err, data2) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let Data2 = JSON.parse(data2)
+      let ambilKataBelakang2 = Data2[0].message.split(' ').slice(-1).join(' ');
+      hasil.push(ambilKataBelakang2);
+      fs.readFile('./data3.json', 'utf8', (err, data3) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        let Data3 = JSON.parse(data3)
+        let ambilKataBelakang3 = Data3[0].data.message.split(' ').slice(-1).join(' ');
+        hasil.push(ambilKataBelakang3);
+        console.log(hasil);
+      });
+    });
+  });
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
